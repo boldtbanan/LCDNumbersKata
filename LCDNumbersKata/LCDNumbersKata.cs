ca693487ba7a2fce9240182842d355166da9a811
boldtbanan <img4@cornell.edu>
2016-08-26 09:31:11 -0500

    Initial version

---
+++ LCDNumbersKata/LCDNumbersKata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LCDNubmersKata
+{
+    // Each number translates to a 3x3 grid of either space, | or _, so:
+    //
+    // 123456789
+    //
+    // translates to:
+    //
+    //   _ _     _    _  _  _
+    // | _|_||_||_ |_  ||_||_|
+    // ||_ _|  | _||_| ||_| _|
+
+    public class LCDNumbersKata
+    {
+        public string Translate(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestClass]
+    public class Tests
+    {
+        public LCDNumbersKata System { get; set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            System = new LCDNumbersKata();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
